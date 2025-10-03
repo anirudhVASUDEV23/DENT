0bd379be18bf6112d07622b615aa7f7cc357741f
anirudhVASUDEV23 <anirudh.vasudev.kesana@gmail.com>
2025-10-03 16:04:00 +0530

    sync-user-uusing-server-actions-and-adding the component in the main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,14 @@ import WhatToAsk from "@/components/landing/WhatToAsk";
 import PricingSection from "@/components/landing/PricingSection";
 import CTA from "@/components/landing/CTA";
 import Footer from "@/components/landing/Footer";
+import { currentUser } from "@clerk/nextjs/server"; //using this because by default this page is server side rendered
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+
+  if (user) redirect("/dashboard");
+
   return (
     <div className="min-h-screen bg-background">
       <Header />
